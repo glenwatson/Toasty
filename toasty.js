6d270d7ded50a7c538f65bfc7684e691dcc69302
Glen Watson <gwatson@indeed.com>
2014-10-10 21:33:10 -0500

    Split into functions, put into module

--- toasty.js
+++ toasty.js
@@ -1,51 +1,106 @@
-function loadToasty()
-{
-	//key recognition
-	var uppercutkeys = [], uppercut = "39,39,38,40";
-	$(document).keydown(function(e)
-	{
-		uppercutkeys.push( e.keyCode );
-		if( uppercutkeys.toString().indexOf( uppercut ) >= 0 )
-		{
+(function(ns) { //namespace
+	
+	//image and audio variables
+	var _toastyimage;
+	var _toastysound;
+	var _toastyimageSrc = 'dan.png';
+	var _toastysoundSrc = ['toasty.mp3', 'toasty.wav', 'Deception_Toasty.mp3'];
+	
+	function _loadToasty() {
+		// make dan
+		_toastyimage = $('<img src="'+_toastyimageSrc+'" />')
+			.css({
+				"position" : "fixed",
+				"bottom" : "-300px",
+				"right" : "-300px"
+			});
+		//make voice
+		var audio = '<audio preload="auto">';
+		for (i in _toastysoundSrc)
+			audio += '<source src="'+_toastysoundSrc[i]+'" />';
+		audio += '</audio>';
+		_toastysound = $(audio);
+
+		$('body').append(_toastyimage).append(_toastysound);
+		ns.uppercut();
+	}
+	
+	var uppercutkeys = [];
+	function _keyDownUppercut(e) {
+		uppercutkeys.unshift( e.keyCode || e.keyCode || e.key );
+		if (uppercutkeys.length > 4) {
+			uppercutkeys = uppercutkeys.slice(0, 4);
+		}
+		if( 	uppercutkeys[0] === 40 && //down
+				uppercutkeys[1] === 38 && //up
+				uppercutkeys[2] === 39 && //right
+				uppercutkeys[3] === 39) { //right
 			//unbind the listener
 			//$(document).unbind('keydown',arguments.callee);
 			//reset the keys
 			uppercutkeys = [];
-			
-			//image and audio variables
-			var toastyimage = $('<img id="bigdan" style="display: none" src="dan.png" />');
-			var toastysound = $('<audio id="toasty" preload="auto"><source src="Deception_Toasty.mp3" /><source src="toasty.mp3" /><source src="toasty.wav" /></audio>');
-			
-			//set the default style of the image to be out of sight
-			$('body').append(toastyimage);
-			$('body').append(toastysound);
-			var danforden = $('#bigdan').css({
-				"position" : "fixed",
-				"bottom" : "-300px",
-				"right" : "-300px",
-				"display" : "block"
-			})
-			
-			//plays the <audio> sound
-			document.getElementById('toasty').play();
-			
-			// pops up dan and then hides him again	
-			danforden.animate({"bottom" : "0","right" : "0"}, 300,
-				function() { 	
-					$('#bigdan').delay(1000).animate({"bottom" : "-300px", "right" : "-300px"}, 300, 
-						function()
-						{
-							//clean-up
-							toastyimage.remove();
-							toastysound.remove();
-						}
-					)
-				}
-			);
-			
-		}          
-	});
-}
+			ns.toasty();
+		}
+	}
+	
+	/*
+	 * Listens for right, right, up, down
+	 */
+	ns.uppercut = function() {
+		$(document).on('keydown', _keyDownUppercut);
+	}
+	
+	/*
+	 * Stops waiting for uppercut
+	 */
+	ns.uppercutOff = function() {
+		$(document).off('keydown', _keyDownUppercut);
+	}
+	
+	/*
+	 * Binds to onclick event
+	 * pass in either a DOM element or a CSS selector as a string
+	 */
+	ns.bind = function(arg) {
+		$(arg).click(ns.toasty);
+	}
+	
+	/*
+	 * Fires the 
+	 * You can call toasty.toasty() to trigger for your own events
+	 */
+	ns.toasty = function() {
+		//plays the <audio> sound
+		_toastysound[0].play();
+	
+		// pops up dan and then hides him again	
+		_toastyimage.animate({"bottom" : "0","right" : "0"}, 300,
+			function() { 	
+				_toastyimage.delay(1000).animate({"bottom" : "-300px", "right" : "-300px"}, 300)
+			}
+		);
+	}
+	
+	/*
+	 * Removes the assests from the page
+	 */
+	ns.remove = function() {
+		_toastyimage.remove();
+		_toastysound.remove();
+	}
+	
+	// ensure jQuery is loaded, then call _loadToasty()
+	if (typeof jQuery === 'undefined') {
+		var j = document.createElement('script');
+		j.type = 'text/javascript';
+		j.src = 'jQuery1.9.0.js';
+		j.onload = _loadToasty;
+		document.getElementsByTagName('head')[0].appendChild(j);
+	} else {
+		_loadToasty();
+	}
+	
+})(window.toasty = window.toasty || {});
 
 // ensure jQuery is loaded, then call loadToasty()
 if (typeof jQuery === 'undefined')
